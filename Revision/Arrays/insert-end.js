2d0a5ab286d3a7b126e9db8367b0bed462748210
Sivananda-Balaji <sivanandabalaji6@gmail.com>
2023-12-01 20:52:54 +0530

    add insert at the end in an array solution

---
+++ Revision/Arrays/insert-end.js
@@ -0,0 +1,12 @@
+//Array insert at end
+
+const insertAtEnd = (arr, sizeOfArray, element) => {
+  arr[sizeOfArray] = element;
+  return arr;
+};
+
+const arr = [1, 2, 3, 4, 5];
+
+const result = insertAtEnd(arr, arr.length, 90);
+
+console.log(result);
